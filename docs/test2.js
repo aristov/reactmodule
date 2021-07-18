d104acab83d0af5e59240feb216d200c8fbdd51b
Vyacheslav Aristov <vv.aristov@gmail.com>
2021-07-18 13:29:56 +0300

    Editing tests

--- docs/test2.js
+++ docs/test2.js
@@ -1,14 +1,28 @@
 import {
-  HtmlImg, HtmlBody, HtmlHeader, HtmlMain, HtmlFooter, HtmlH1,
+  HtmlImg, HtmlBody, HtmlHeader, HtmlMain, HtmlFooter, HtmlH1, HtmlButton,
 } from '../lib'
 import './index.css'
 
 class SiteBody extends HtmlBody
 {
+  constructor(props) {
+    super(props)
+    this.state = { crazy : false }
+  }
+
   render() {
     return [
-      new HtmlHeader(new HtmlImg({ src : 'crazy7.jpg', alt : 'UserPic' })),
-      new HtmlMain(new HtmlH1('Vyacheslav Aristov')),
+      new HtmlHeader(new HtmlImg({
+        src : this.state.crazy? 'crazy7.jpg' : 'aristov.jpg',
+        alt : 'UserPic',
+      })),
+      new HtmlMain([
+        new HtmlH1('Vyacheslav Aristov'),
+        new HtmlButton({
+          onclick : () => this.setState({ crazy : !this.state.crazy }),
+          children : 'Toggle the userpic',
+        }),
+      ]),
       new HtmlFooter(['@ ', (new Date).getFullYear(), ' Vyacheslav Aristov']),
     ]
   }
