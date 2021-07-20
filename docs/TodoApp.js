a99cde94c430c556fc86dacf898c2e14610c3ad1
Vyacheslav Aristov <vv.aristov@gmail.com>
2021-07-20 21:52:33 +0300

    Component.componentDidMount

--- docs/TodoApp.js
+++ docs/TodoApp.js
@@ -5,15 +5,22 @@ export class TodoApp extends HtmlDiv
 {
   constructor(props) {
     super(props)
-    this.state = { items : api.getItems() }
+    this.state = { items : null }
     api.addEventListener('update', this.onUpdate)
   }
 
   render() {
+    if(!this.state.items) {
+      return new HtmlDiv('Загрузка...')
+    }
     return [
       new TodoForm,
       new TodoList({ items : this.state.items }),
     ]
+  }
+
+  async componentDidMount() {
+    this.setState({ items : await api.getItems() })
   }
 
   onUpdate = e => {
@@ -28,21 +35,29 @@ export class TodoApp extends HtmlDiv
 
 class TodoForm extends HtmlForm
 {
+  constructor(props) {
+    super(props)
+    this.state = { busy : false }
+  }
+
   render() {
+    const disabled = this.state.busy
     this.on('submit', this.onSubmit)
     return [
-      this._input = new HtmlInput({ required : true }),
-      new HtmlButton('Добавить'),
+      this._input = new HtmlInput({ required : true, disabled }),
+      this._button = new HtmlButton({ children : 'Добавить', disabled }),
     ]
   }
 
-  onSubmit = e => {
+  onSubmit = async e => {
     e.preventDefault()
-    api.createItem({
+    this.setState({ busy : true })
+    await api.createItem({
       text : this._input.node.value.trim(),
       completed : false,
     })
     this._input.node.value = ''
+    this.setState({ busy : false })
   }
 }
 
@@ -55,69 +70,88 @@ class TodoList extends HtmlUl
 
 class TodoItem extends HtmlLi
 {
+  constructor(props) {
+    super(props)
+    this.state = { busy : false }
+  }
+
   render() {
     const item = this.props.item
+    const busy = this.state.busy
+    this.node.setAttribute('aria-busy', busy)
     return [
       this._checkbox = new HtmlInput({
         type : 'checkbox',
         defaultChecked : item.completed,
-        onchange : () => api.updateItem({
-          id : item.id,
-          completed : this._checkbox.node.checked,
-        }),
+        disabled : busy,
+        onchange : this.onChange,
       }),
       new HtmlDiv(item.text),
       new HtmlButton({
         children : 'Удалить',
-        onclick : () => api.removeItem(item.id),
+        disabled : busy,
+        onclick : this.onRemove,
       }),
     ]
   }
+
+  onChange = async () => {
+    this.setState({ busy : true })
+    await api.updateItem({
+      id : this.props.item.id,
+      completed : this._checkbox.node.checked,
+    })
+    this.setState({ busy : false })
+  }
+
+  onRemove = async () => {
+    this.setState({ busy : true })
+    await api.removeItem(this.props.item.id)
+  }
 }
+
+const DELAY = 500
 
 class AppInteface extends EventTarget
 {
   constructor() {
     super()
-    this._load()
+    const json = localStorage.getItem('TodoApp.items')
+    this._data = json? JSON.parse(json) : []
   }
 
-  _load() {
-    const data = localStorage.getItem('TodoApp.items')
-    return this._data = data? JSON.parse(data) : []
-  }
-
-  _save() {
+  async _save() {
     localStorage.setItem('TodoApp.items', JSON.stringify(this._data))
+    await new Promise(resolve => setTimeout(resolve, DELAY))
     this.dispatchEvent(new CustomEvent('update', { detail : this._data.slice() }))
   }
 
-  getItems() {
-    return this._data.slice()
+  async getItems() {
+    return new Promise(resolve => setTimeout(() => resolve(this._data.slice()), DELAY))
   }
 
-  createItem(item) {
+  async createItem(item) {
     item.id = Date.now()
     this._data.push(item)
     // this._data.unshift(item)
-    this._save()
+    return this._save()
   }
 
-  updateItem(newItem) {
+  async updateItem(newItem) {
     const item = this._data.find(item => item.id === newItem.id)
     Object.assign(item, newItem)
-    this._save()
+    return this._save()
   }
 
-  removeItem(id) {
+  async removeItem(id) {
     this._data = this._data.filter(item => item.id !== id)
-    this._save()
+    return this._save()
   }
 
   reorderItems(type) {
     type === 'pop' && this._data.unshift(this._data.pop())
     type === 'push' && this._data.push(this._data.shift())
-    this._save()
+    void this._save()
     // return this._data
   }
 }
