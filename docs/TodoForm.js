180fb857d77eb79bc78d1d9d21e0796709ca32bd
Vyacheslav Aristov <vv.aristov@gmail.com>
2022-05-04 22:43:04 +0300

    TodoApp: refactoring

---
+++ docs/TodoForm.js
@@ -0,0 +1,36 @@
+import { HtmlForm, HtmlInput, HtmlButton } from '../lib'
+import api from './api'
+
+export class TodoForm extends HtmlForm
+{
+  getState() {
+    return { text : '', busy : false }
+  }
+
+  render() {
+    this.onsubmit = this.onSubmit
+    return [
+      this._input = new HtmlInput({
+        disabled : this.state.busy,
+        required : true,
+        value : this.state.text,
+        oninput : e => this.setState({ text : e.target.value }),
+      }),
+      new HtmlButton({
+        disabled : this.state.busy,
+        children : 'Add',
+      }),
+    ]
+  }
+
+  onSubmit = async e => {
+    e.preventDefault()
+    this.setState({ busy : true })
+    await api.createItem({
+      text : this.state.text.trim(),
+      completed : false,
+    })
+    this.setState({ text : '', busy : false })
+    this._input.node.focus()
+  }
+}
---
+++ docs/TodoItem.js
@@ -0,0 +1,43 @@
+import { HtmlLi, HtmlInput, HtmlDiv, HtmlButton } from '../lib'
+import api from './api'
+
+export class TodoItem extends HtmlLi
+{
+  getState() {
+    return { busy : false }
+  }
+
+  render() {
+    const item = this.props.item
+    this.node.setAttribute('aria-busy', this.state.busy)
+    return [
+      new HtmlInput({
+        type : 'checkbox',
+        checked : item.completed,
+        disabled : this.state.busy,
+        onchange : this.onChange.bind(this),
+      }),
+      new HtmlDiv(item.text),
+      new HtmlButton({
+        children : 'Delete',
+        disabled : this.state.busy,
+        onclick : e => this.onRemove(e),
+      }),
+    ]
+  }
+
+  async onChange() {
+    const item = this.props.item
+    this.setState({ busy : true })
+    await api.updateItem({
+      id : item.id,
+      completed : !item.completed,
+    })
+    this.setState({ busy : false })
+  }
+
+  onRemove = async () => {
+    this.setState({ busy : true })
+    await api.removeItem(this.props.item.id)
+  }
+}
